213c5625e38b4fb1adc6487b803d9193fa411485
kurim <git@kurim.de>
2023-10-28 20:57:30 +0200

    initial

---
+++ config/db-config-sample.php
@@ -0,0 +1,11 @@
+<?php
+// db-config.php
+
+return [
+    'dbname' => 'filament_info',
+    'user' => 'filament',
+    'password' => '',
+    'host' => 'localhost',
+    'driver' => 'pdo_mysql',
+    'charset'  => 'utf8',
+];
