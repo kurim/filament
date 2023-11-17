d2cd673af08ddd91ab177a5a9230e3a4d083175b
kurim <git@kurim.de>
2023-11-17 08:58:17 +0100

    remove no needed code and update style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./**/*.{html,js,twig,html.twig}",
-      "./templates/**/*.{html,js,twig,html.twig}",
-      ],
-    theme: {
-      extend: {
-        animation: {
-          gradient: 'gradient 3s linear infinite',
-        },
-        keyframes: {
-          gradient: {
-            '0%': { backgroundPosition: '0% 50%' },
-            '100%': { backgroundPosition: '100% 50%' },
-          },
-        },
-        backgroundImage: {
-          'gradient-x-blue-500': 'linear-gradient(90deg, #0000FF, #00FFFF)',
+  content: [
+    "./**/*.{html,js,twig,html.twig}",
+    "./templates/**/*.{html,js,twig,html.twig}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        gradient: "gradient 3s linear infinite",
+      },
+      keyframes: {
+        gradient: {
+          "0%": { backgroundPosition: "0% 50%" },
+          "100%": { backgroundPosition: "100% 50%" },
         },
       },
+      backgroundImage: {
+        "gradient-x-blue-500": "linear-gradient(90deg, #0000FF, #00FFFF)",
+      },
     },
-    plugins: []
-  }
+  },
+  plugins: [],
+};
